ad113515a9bd275a8e1fd8135bd7e98956f32e89
joshtom <olajidejoshua4real@gmail.com>
2020-06-12 19:04:12 +0100

    I update Navigation bar styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./styles.module.css";
-import Sunlight from "../../assets/sunlight.svg"
+import { TweenMax } from "gsap";
+import Sunlight from "../../assets/sunlight.svg";
+
 
 export default function Navigation() {
+    const navToggle = React.useRef(null);
+    const handleNavClick = () => {
+        TweenMax.to(navToggle.current, .4, 
+            {   x: 0, 
+                opacity: 1,
+                ease: "slow(0.7, 0.7, false)" 
+            });
+    }
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
@@ -12,12 +22,19 @@ export default function Navigation() {
                 <div className={styles.themeToggler}>
                     <Sunlight />
                 </div>
-                <div className={styles.hamburger}>
+                <div 
+                className={styles.hamburger}
+                onClick={handleNavClick}
+                >
                     <div></div>
                     <div></div>
                     <div></div>
                 </div>
             </nav>
+
+            <div className={styles.navToggle} ref={navToggle}>
+
+            </div>
         </header>
 
 

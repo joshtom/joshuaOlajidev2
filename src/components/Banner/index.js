bf5fdf22621d0f648d7e141934537b783481076b
joshtom <olajidejoshua4real@gmail.com>
2020-06-13 15:56:42 +0100

    Update Nav component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./styles.module.css";
+import Moon from "../../assets/moon.svg";
 
 
 export default function Banner () {
     return(
         <div className={styles.banner}>
             <section>
-
+            <div className={styles.scroll}>
+               <div><Moon /></div> <div>scroll</div>
+            </div>
             </section>
 
             <section>
@@ -17,6 +20,7 @@ export default function Banner () {
                 <div>JOS</div>
                 <div>HUA <span className={styles.dot}></span></div>
             </h1>
+            
         </div>
     )
 }

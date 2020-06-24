3a19315b612bbf4b726c1e18a0c9abca378934d7
joshtom <olajidejoshua4real@gmail.com>
2020-06-24 22:14:55 +0100

    I complete navigation menu

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,124 +1,118 @@
-import React from "react";
-import styles from "./styles.module.css";
+import React, { useRef, useState, useEffect } from "react";
+import "./styles.css";
 import { TimelineLite, TweenLite } from "gsap";
 import Sunlight from "../../assets/sunlight.svg";
-import Close from "../../assets/close.svg";
+// import Close from "../../assets/close.svg";
 
 
 export default function Navigation() {
-    const [ isToggled, setToggled ] = React.useState(false)
-    const tl = new TimelineLite({paused: true, repeat: false});
-    const animateHamburger = TweenLite;
-    const stick1 = React.useRef(null);
-    const stick2 = React.useRef(null);
-    const stick3 = React.useRef(null);
-    const navToggle = React.useRef(null);
-    const navToggleLinks = React.useRef(null);
-    const header = React.useRef(null);
-    const aboutLink = React.useRef(null);
-    const writingLink = React.useRef(null);
-    const projectLink = React.useRef(null);
-    const contactLink = React.useRef(null);
-    const socialLink = React.useRef(null);
-    const closeDiv = React.useRef(null);
+    const [ isToggled, setToggled ] = useState(false)
+    const tl = useRef();
+    // const animateHamburger = TweenLite;
+    const navToggle = useRef(null);
+    const navToggleLinks = useRef(null);
+    const header = useRef(null);
+    const Link = useRef(null);
+    const socialLink = useRef(null);
+
+    useEffect(() => {
+        tl.current = new TimelineLite({paused: true});
+        tl.current.to(navToggle.current, .4, 
+            {
+                x: 0,
+                opacity: 1,
+                ease: "back.out(1.7)"
+            })
+            .fromTo(socialLink.current,  .4,{ y: 10, opacity: 0 }, { y: 0, opacity: 1 })
+            .play();
+            document.body.style.overflow = "hidden";
+    }, []);
+
+    // useEffect(() => {
+    //     if (navToggleLinks.current.contains(e.target)) {
+    //         setToggled(false)
+    //         tl.current.reverse();
+    //       }
+       
+    // }, [])
 
 
-    const handleNavClick = () => {
-          setToggled(!isToggled)
-
-          if(isToggled === false) {
-            // TweenLite.to(stick3.current, .2, {opacity: 0} );
-            // TweenLite.to(stick1.current, .2, {
-            //     y: 6,
-            //     rotate: 50
-            // });
-            // TweenLite.to(stick2.current, .2, {
-            //     rotate: -50,
-            // });
-
-            tl.to(navToggle.current, .4, 
-                {
-                    x: 0,
-                    opacity: 1,
-                    ease: "back.out(1.7)"
-                })
-                .fromTo(aboutLink.current,   .3,  { y: 10, opacity: 0 }, { y: 0, opacity: 1 })
-                .fromTo(writingLink.current, .3,{ y: 10, opacity: 0 }, { y: 0, opacity: 1 })
-                .fromTo(projectLink.current, .3,{ y: 10, opacity: 0 }, { y: 0, opacity: 1 })
-                .fromTo(contactLink.current, .3,{ y: 10, opacity: 0 }, { y: 0, opacity: 1 })
-                .fromTo(socialLink.current,  .4,{ y: 10, opacity: 0 }, { y: 0, opacity: 1 })
-                .fromTo(closeDiv.current,  .4,{ x: 100, opacity: 0 }, { x: 0, opacity: 1 })
-                .play();
-          } else if(isToggled === true) {
-            tl.to(navToggle.current, .4, { x: 1000, opacity: 0 });
-          }
-    }
+    useEffect(() => {
+        isToggled ? tl.current.play() : tl.current.reverse(); document.body.style.overflow = "none";;
+      }, [isToggled]);
 
 
-
-    const handleCloseClick = (e) => {
-        if (navToggleLinks.current.contains(e.target)) {
-            return;
-          }
-        tl.to(navToggle.current, .4, { x: 1000, opacity: 0 });
-    }
-
-    React.useEffect(() => {
-        // add when mounted
-        document.addEventListener("mousedown", handleCloseClick);
-        // return function to be called when unmounted
-        return () => {
-          document.removeEventListener("mousedown", handleCloseClick);
-        };
-      }, []);
+    // React.useEffect(() => {
+    //     // add when mounted
+    //     document.body.addEventListener("click", () => {
+    //         tl.current.reverse()
+    //         setToggled(false)
+    //     });
+    //     // return function to be called when unmounted
+    //     return () => {
+    //       document.body.removeEventListener("click", () => {
+    //           tl.current.reverse()
+    //             setToggled(false)
+    //       });
+    //     };
+    //   }, []);
 
     return (
-        <header className={styles.header} ref={header}>
-            <nav className={styles.nav}>
-                <div className={styles.logo}>
+        <header className="header" ref={header}>
+            <nav className="nav">
+                <div className="logo">
                 JO
                 </div>
-                <div className={styles.themeToggler}>
+                <div className="themeToggler">
                     <Sunlight />
                 </div>
                 <div 
-                className={styles.hamburger}
-                onClick={handleNavClick}
+                className={`hamburger ${isToggled ? 'animate__hamburger' : ''}`}
+                onClick={() => setToggled(!isToggled)}
                 role="button"
-                tabIndex="1"
                 >
-                    <div ref={stick1}></div>
-                    <div ref={stick2}></div>
-                    <div ref={stick3}></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
             </nav>
                 <div 
-                className={styles.navToggle} 
+                className="navToggle" 
                 ref={navToggle}
                 >
-                            <ul className={styles.link} ref={navToggleLinks}>
-                            <li ref={aboutLink}>
+                            <ul className="link" ref={navToggleLinks}>
+                            <li ref={Link}
+                            onClick={() => setToggled(!isToggled)}
+                            >
                                 <a href="#" id="about">About</a>
                             </li>
         
-                            <li ref={writingLink}>
+                            <li ref={Link}
+                            onClick={() => setToggled(!isToggled)}
+                            >
                                 <a href="#" id="pen">Writings</a>
                             </li>
         
-                            <li ref={projectLink}>
+                            <li ref={Link}
+                            onClick={() => setToggled(!isToggled)}
+                            >
                                 <a href="#" id="project">Project</a>
                             </li>
         
-                            <li ref={contactLink}>
+                            <li ref={Link}
+                            onClick={() => setToggled(!isToggled)}
+                            >
                                 <a href="#" id="contact">Contact</a>
                             </li>
         
-                            <li ref={socialLink}>
-                                <div className={styles.social}>
-                                    <a href="#" className={styles.gh}>GH</a>
-                                    <a href="#" className={styles.tw}>TW</a>
-                                    <a href="#" className={styles.ln}>LN</a>
-                                    <a href="#" className={styles.md}>MD</a>
+                            <li ref={socialLink}
+                            onClick={() => setToggled(!isToggled)}
+                            >
+                                <div className="social">
+                                    <a href="#" className="gh">GH</a>
+                                    <a href="#" className="tw">TW</a>
+                                    <a href="#" className="ln">LN</a>
+                                    <a href="#" className="md">MD</a>
                                 </div>
                             </li>
                         </ul>

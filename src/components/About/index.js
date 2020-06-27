68f39842d91bdce2a1a2debcc7d0f7967eb7df4a
joshtom <olajidejoshua4real@gmail.com>
2020-06-27 20:14:43 +0100

    Fixed overflow issue on about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,12 +5,12 @@ export default function About() {
     return(
         <div className="About">
             <div>
-                <h1>Over the past few years,</h1>   
+                <h1>Over the past few years,</h1>  
                 <p>
-                I am an experienced User Interface Designer, Javascript Developer, and Life Long Learner. I live a simple and optimistic life and I always put on a smiling face. When I am not in front of my computer, I'm either playing Game or playing piano and enjoying the Harmony. <br />
-                Over the past few years I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies.
-                <br />
-                I am passionate about web performance, Javascript applications, Great user experiences, and Open Source #Linux, and i'm also open for consultation, remote or contract work.
+                I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies. Not only these makes me standout, but paying attention to every detail from any DESIGN which makes me deliver clean, elegant and pixel-perfect solutions.
+                </p> 
+                <p>
+                I am passionate about Web Performance, Javascript Applications, Great user experiences, and Open Source. I am also open for remote or contract work.
                 </p>
             </div>
             <div>

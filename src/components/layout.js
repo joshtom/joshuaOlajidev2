765f5eb7e48fca98caf2f684421c40558225f8ef
joshtom <olajidejoshua4real@gmail.com>
2020-07-31 22:35:45 +0100

    Initial commit from gatsby: (https://github.com/alexandreramosdev/gatsby-starter-default-dark-mode.git)

---
+++ src/components/layout.js
@@ -0,0 +1,50 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import { ThemeProvider } from "styled-components"
+
+import GlobalStyle from "../styles/globalStyles"
+import { theme as styledTheme } from "../styles/theme"
+
+import Header from "./header"
+import { Container } from "./container"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <ThemeProvider theme={styledTheme}>
+      <GlobalStyle />
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <Container>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout

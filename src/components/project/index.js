f7f1a062db4bbd0fc361c07efcbf544f68c5d11c
joshtom <olajidejoshua4real@gmail.com>
2020-08-11 16:16:31 +0100

    Filled all the cards with content

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -36,22 +36,47 @@ const Project = ({ data }) =>{
                 <div className="image-links">
                     <Github />
                     <Url className="url"/>
-                </div>
+                </div>  
            </div>
            <div className="card">
-           
+                <h1> Feeling <br /> Pressed.</h1>
+                <p> blog </p>
+                <div className="image-links">
+                    <Github />
+                    <Url className="url"/>
+                </div>  
            </div>
            <div className="card">
-           
+                <h1> Serv- <br /> iceMart.</h1>
+                <p> investment </p>
+                <div className="image-links">
+                    <Github />
+                    <Url className="url"/>
+                </div>  
            </div>
            <div className="card" ref={card}>
-           
+                 <h1> Dev <br /> Alert.</h1>
+                <p> jobs </p>
+                <div className="image-links">
+                    <Github />
+                    <Url className="url"/>
+                </div>  
            </div>
            <div className="card" ref={cards}>
-          
+                  <h1> Covid <br /> Report.</h1>
+                <p> healthcare </p>
+                <div className="image-links">
+                    <Github />
+                    <Url className="url"/>
+                </div>  
            </div>
            <div className="card" ref={cardss}>
-          
+                 <h1> Conn- <br /> ect Four.</h1>
+                <p> fun game </p>
+                <div className="image-links">
+                    <Github />
+                    <Url className="url"/>
+                </div>  
            </div>
         </div>
         <center>

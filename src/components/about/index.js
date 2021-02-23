57cf4829cfc2b22b96981c51a375278e8778dfa5
joshtom <olajidejoshua4real@gmail.com>
2021-02-23 17:39:46 +0100

    Update onboarding animation

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,53 +1,66 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { Image } from './profileImg'
-import "./style.scss";
+import { Image } from "./profileImg"
+import "./style.scss"
 
 export default function About() {
-        const aboutContainer = useRef(null);
-        const image = useRef(null);
-        const about = useRef(null);
+  const aboutContainer = useRef(null)
+  const image = useRef(null)
+  const about = useRef(null)
 
-     useEffect(() => {
-        if (typeof window !== `undefined`) {
-            gsap.registerPlugin(ScrollTrigger)
-            gsap.core.globals('ScrollTrigger', ScrollTrigger)
-            }
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      gsap.registerPlugin(ScrollTrigger)
+      gsap.core.globals("ScrollTrigger", ScrollTrigger)
+    }
 
-            ScrollTrigger.matchMedia({
-                "(min-width: 992px)": function scrollOnMobile() {
-                    
-                    const tl = gsap.timeline({
-                        paused: true,
-                        scrollTrigger: {
-                        trigger: aboutContainer.current,
-                        start: "top top",
-                        scrub: true,
-                        }
-                    });
+    ScrollTrigger.matchMedia({
+      "(min-width: 992px)": function scrollOnMobile() {
+        const tl = gsap.timeline({
+          paused: true,
+          scrollTrigger: {
+            trigger: aboutContainer.current,
+            start: "top top",
+            scrub: true,
+          },
+        })
 
-                    tl.to([image.current, about.current], .1, {rotation:45, autoAlpha: 0, translateX: 200})
-                        }
-                
-            })
-    }, [])
-    return(
-        <div className="About" ref={aboutContainer} id="about">
-            <div ref={about}>
-                <h1>In the last few years,</h1>  
-                <p>
-                I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies. Not only these makes me standout, but paying attention to every detail from any DESIGN which makes me deliver clean, elegant and pixel-perfect solutions.
-                </p> 
-                <p>
-                I am passionate about Web Performance, Javascript Applications, Great user experiences, and Open Source. I am also open for onsite, remote or contract work.
-                </p>
-            </div>
-            <div>
-                <div className="about__image" ref={image}>
-                <Image />
-                </div>
-            </div>
+        // tl.to([image.current, about.current], 0.1, {
+        //   rotation: 45,
+        //   autoAlpha: 0,
+        //   translateX: 200,
+        // })
+        tl.to(image.current, 0.1, {
+          translateX: 200,
+        }).to(about.current, 0.1, {
+          translateX: -200,
+        })
+      },
+    })
+  }, [])
+  return (
+    <div className="About" ref={aboutContainer} id="about">
+      <div ref={about}>
+        <h1>In the last few years,</h1>
+        <p>
+          I've built products that solve real-life problems ranging from
+          Businesses to companies with focus on creating fast, and accessible
+          user experiences using advanced web technologies. Not only these makes
+          me standout, but paying attention to every detail from any DESIGN
+          which makes me deliver clean, elegant and pixel-perfect solutions.
+        </p>
+        <p>
+          I am passionate about Web Performance, Javascript Applications, Great
+          user experiences, and Open Source. I am also open for onsite, remote
+          or contract work.
+        </p>
+      </div>
+      <div>
+        <div className="about__image" ref={image}>
+          <Image />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/components/more/index.js
+++ src/components/more/index.js
@@ -1,56 +1,64 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import "./style.scss";
+import "./style.scss"
 
 export default function More() {
-        const moreContainer = useRef(null)
-        const iDesign = useRef(null);
-        const iDevelop = useRef(null);
-        const iLearn = useRef(null);
-    useEffect(() => {
-        if (typeof window !== `undefined`) {
-            gsap.registerPlugin(ScrollTrigger)
-            gsap.core.globals('ScrollTrigger', ScrollTrigger)
-            }
+  const moreContainer = useRef(null)
+  const iDesign = useRef(null)
+  const iDevelop = useRef(null)
+  const iLearn = useRef(null)
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      gsap.registerPlugin(ScrollTrigger)
+      gsap.core.globals("ScrollTrigger", ScrollTrigger)
+    }
 
-            ScrollTrigger.matchMedia({
-                "(min-width: 992px)": function scrollOnMobile() {
-                    const tl = gsap.timeline({
-                        scrollTrigger: {
-                          trigger: moreContainer.current,
-                          scrub: true,
-                        }
-                      });
-        
-                      tl.fromTo(iDesign.current, .1, { opacity: 0, y: 100 }, { opacity: 1, y: 0})
-                      .fromTo(iDevelop.current, .1, { opacity: 0, y: 100 }, { opacity: 1, y: 0})
-                      .fromTo(iLearn.current, .1, { opacity: 0, y: 100 }, { opacity: 1, y: 0})
-                        }
-                
-            })
-    }, [])
+    ScrollTrigger.matchMedia({
+      "(min-width: 992px)": function scrollOnMobile() {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: moreContainer.current,
+            // scrub: true,
+          },
+        })
 
-    return(
-        <div className="more" id="more" ref={moreContainer}>
-        <div ref={iDesign}>
-            <h1>iDesign. </h1>
-            <p>
-                I may not be the guy behind Figma or AdobeXD, but I can take any design to the next level with my stylesheet superpowers (o _ >)
-            </p>
-        </div>
-        <div ref={iDevelop}>
-            <h1>iDevelop..</h1>
-            <p>
-                I use the latest Frontend-end technologies 👩🏼‍💻 to build efficient, scalable, and super fast solutions ranging from small to large organizations.
-            </p>
-        </div>
-        <div ref={iLearn}>
-            <h1>iLearn...</h1>
-            <p>
-                I strongly believe that learning 📑is a part of the journey. The more I learn new stuffs, the more I am able to make connections of the things around me.
-            </p>
-        </div>
+        tl.fromTo(iDesign.current, { opacity: 0, x: 200 }, { opacity: 1, x: 0 })
+          .fromTo(
+            iDevelop.current,
+            { opacity: 0, x: -200 },
+            { opacity: 1, x: 0 }
+          )
+          .fromTo(iLearn.current, { opacity: 0, y: 200 }, { opacity: 1, y: 0 })
+      },
+    })
+  }, [])
+
+  return (
+    <div className="more" id="more" ref={moreContainer}>
+      <div ref={iDesign}>
+        <h1>iDesign. </h1>
+        <p>
+          I may not be the guy behind Figma or AdobeXD, but I can take any
+          design to the next level with my stylesheet superpowers {`(o _ >)`}
+        </p>
+      </div>
+      <div ref={iDevelop}>
+        <h1>iDevelop..</h1>
+        <p>
+          I use the latest Frontend-end technologies 👩🏼‍💻 to build efficient,
+          scalable, and super fast solutions ranging from small to large
+          organizations.
+        </p>
+      </div>
+      <div ref={iLearn}>
+        <h1>iLearn...</h1>
+        <p>
+          I strongly believe that learning 📑is a part of the journey. The more
+          I learn new stuffs, the more I am able to make connections of the
+          things around me.
+        </p>
+      </div>
     </div>
-    )
+  )
 }
